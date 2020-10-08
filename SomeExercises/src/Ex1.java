934ede55d9e8a2b4b1ea3d08603327d33b4d77c2
raczkowskir <raczkowskir@yahoo.com>
2020-10-08 21:26:13 +0200

    Initial commit

---
+++ SomeExercises/src/Ex1.java
@@ -0,0 +1,26 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        System.out.println("Type a number:");
+        Ex1 ex1 = new Ex1();
+        System.out.println(Integer.toBinaryString(ex1.getNumber()));
+    }
+
+    public Integer getNumber() {
+        Integer myNumber;
+        while(true) {
+            try {
+                Scanner scanner = new Scanner(System.in);
+                myNumber = scanner.nextInt();
+            } catch (InputMismatchException E) {
+                System.out.println("This is not an integer number. Type the number one more time.");
+                continue;
+            }
+            return myNumber;
+        }
+    }
+
+
+}
