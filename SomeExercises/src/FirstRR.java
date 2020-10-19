51a342c1e83f60d762a570537ff8446d719bfe50
raczkowskir <raczkowskir@yahoo.com>
2020-10-19 21:40:52 +0200

    some fight with setting icon :(

--- SomeExercises/src/FirstRR.java
+++ SomeExercises/src/FirstRR.java
@@ -1,10 +1,28 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class FirstRR {
     private JButton resultButton;
     private JPanel panel1;
+    private JButton closeButton;
+    private Icon icon = new Icon() {
+        @Override
+        public void paintIcon(Component c, Graphics g, int x, int y) {
+
+        }
+
+        @Override
+        public int getIconWidth() {
+            return 30;
+        }
+
+        @Override
+        public int getIconHeight() {
+            return 30;
+        }
+    }
 
     public FirstRR() {
         resultButton.addActionListener(new ActionListener() {
@@ -12,6 +30,15 @@ public class FirstRR {
             public void actionPerformed(ActionEvent e) {
                 System.out.println("Button works!");
                 JOptionPane.showMessageDialog(null, "You actually do sth :)");
+            }
+        });
+
+        closeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("This button works even better :)");
+                JOptionPane.showMessageDialog(null, "You actually do sth :)");
+                closeButton.setIcon(icon);
             }
         });
     }
