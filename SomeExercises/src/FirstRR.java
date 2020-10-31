85b88e3157a385455883740bec5b76f5ee378e15
raczkowskir <raczkowskir@yahoo.com>
2020-10-31 15:10:37 +0100

    First window, fun with buttons and layout.

--- SomeExercises/src/FirstRR.java
+++ SomeExercises/src/FirstRR.java
@@ -1,53 +1,41 @@
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class FirstRR {
-    private JButton resultButton;
-    private JPanel panel1;
-    private JButton closeButton;
-    private Icon icon = new Icon() {
-        @Override
-        public void paintIcon(Component c, Graphics g, int x, int y) {
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JPanel RakiPanel;
 
-        }
+    ActionListener[] Al = new ActionListener[3];
+    JButton[] buttons = {button1, button2, button3};
 
-        @Override
-        public int getIconWidth() {
-            return 30;
-        }
-
-        @Override
-        public int getIconHeight() {
-            return 30;
-        }
+    private void actionsForButtons(String myConsolext, String myMessage) {
+        System.out.println(myConsolext);
+        JOptionPane.showMessageDialog(null, myMessage);
     }
 
     public FirstRR() {
-        resultButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Button works!");
-                JOptionPane.showMessageDialog(null, "You actually do sth :)");
-            }
-        });
+        for (int i = 0; i < Al.length; i++) {
+            String iterator = "" + (i + 1);
+            Al[i] = new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    actionsForButtons("Button " + iterator + " works", "Action " + iterator);
+                }
+            };
 
-        closeButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("This button works even better :)");
-                JOptionPane.showMessageDialog(null, "You actually do sth :)");
-                closeButton.setIcon(icon);
-            }
-        });
+            buttons[i].addActionListener(Al[i]);
+        }
     }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("FirstRR");
-        frame.setContentPane(new FirstRR().panel1);
+        frame.setContentPane(new FirstRR().RakiPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+        frame.setLocation(300, 300);
     }
 }
