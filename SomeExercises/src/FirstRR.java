546e9753cae26c1098c4e9dd0c30ae623c1ba1fb
raczkowskir <raczkowskir@yahoo.com>
2020-11-01 18:38:57 +0100

    Exploring grid layout

--- SomeExercises/src/FirstRR.java
+++ SomeExercises/src/FirstRR.java
@@ -3,32 +3,34 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class FirstRR {
-    private JButton button1;
     private JButton button2;
-    private JButton button3;
     private JPanel RakiPanel;
+    private JTextField aTextField;
+    private JButton button1;
+    private JFormattedTextField mySecondTextField;
 
-    ActionListener[] Al = new ActionListener[3];
-    JButton[] buttons = {button1, button2, button3};
+//    ActionListener[] Al = new ActionListener[3];
+//    JButton[] buttons = {button1, button2, button3};
 
-    private void actionsForButtons(String myConsolext, String myMessage) {
-        System.out.println(myConsolext);
-        JOptionPane.showMessageDialog(null, myMessage);
-    }
+//    private void actionsForButtons(String myConsolext, String myMessage) {
+//        System.out.println(myConsolext);
+//        JOptionPane.showMessageDialog(null, myMessage);
+//        mySecondTextField.setText("Does it work?");
+//    }
 
-    public FirstRR() {
-        for (int i = 0; i < Al.length; i++) {
-            String iterator = "" + (i + 1);
-            Al[i] = new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    actionsForButtons("Button " + iterator + " works", "Action " + iterator);
-                }
-            };
-
-            buttons[i].addActionListener(Al[i]);
-        }
-    }
+//    public FirstRR() {
+//        for (int i = 0; i < Al.length; i++) {
+//            String iterator = "" + (i + 1);
+//            Al[i] = new ActionListener() {
+//                @Override
+//                public void actionPerformed(ActionEvent e) {
+//                    actionsForButtons("Button " + iterator + " works", "Action " + iterator);
+//                }
+//            };
+//
+//            buttons[i].addActionListener(Al[i]);
+//        }
+//    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("FirstRR");
@@ -38,4 +40,5 @@ public class FirstRR {
         frame.setVisible(true);
         frame.setLocation(300, 300);
     }
+
 }
